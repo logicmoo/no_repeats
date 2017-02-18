21940702fcb5b4f66c2da5435ccb088d2e1e3274
root <logicmoo@gmail.com>
2017-02-18 11:31:18 -0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # no_repeats
 New ways to avoid duplicate solutions
+
+
+```prolog
+
+:- pack_install('https://github.com/TeamSPoon/no_repeats.git').
+
+```
+
+
+```prolog
+
+?- use_module(library(no_repeats)).
+
+?- no_repeats( X , member(X-Y,[3-2,1-4,1-5,2-1])).
+% X = 3, Y = 2 ;
+% X = 1, Y = 4 ;
+% X = 2, Y = 1.
+
+
+ ?- no_repeats(member(X,[3,1,1,1,3,2])).
+% X = 3 ;
+% X = 1 ;
+% X = 2.
+
+
+```
+
+
+
---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(no_repeats).
+version('0.0.1').
+title('New ways to avoid duplicate solutions').
+keywords([tools,aggregation]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+home( 'https://github.com/TeamSPoon/no_repeats' ).
+download( 'https://github.com/TeamSPoon/no_repeats/release/*.zip' ).
+
