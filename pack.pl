5ae98c7d24ae307aecf82f600c4ba4aacaebdcb7
root <logicmoo@gmail.com>
2017-03-08 17:32:33 -0800

    v1.1.111

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(no_repeats).
-version('0.0.8').
+version('1.1.111').
 title('New ways to avoid duplicate solutions').
 keywords([tools,aggregation]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
